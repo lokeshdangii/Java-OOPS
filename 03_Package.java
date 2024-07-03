5f9829cb134473d0e9b170e64c9bcc087a677732
lokeshdangii <lokeshdangi1045@gmail.com>
2024-07-03 09:08:25 +0530

    Packages in Java

---
+++ 03_Package.java
@@ -0,0 +1,21 @@
+/*
+    Inside any java program at most one package statemet is allowed.
+
+    if want to use package and import statement together in a java program then package statement must be used before the import statement. Order is very important.
+    If package is placed after the import statement it will give CE.
+    
+    Syntax :-
+
+    package statement
+    import statement
+
+    class/enum/interface {
+
+    }
+ */
+
+// package pack1;
+// package pack2;
+class Test{
+
+}
