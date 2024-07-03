fdae57f182e395e1ea71a02431727df7f525cd67
lokeshdangii <lokeshdangi1045@gmail.com>
2024-07-03 09:08:39 +0530

    Class Level Modifier

---
+++ 04_Class_Level_Modifier.java
@@ -0,0 +1,30 @@
+
+/*
+Class leve modifier
+:-  By using appropriate modifier we can describe the behaviour of our required class.
+    We can specify the information of our class to the JVM by corresponding class modifiers
+
+(1) Accessibilty :- 
+    --> If a class is public we can access it from anywhere i.e from within a package, outside a package, no restrictions at all. 
+    --> When a class is not public it is default class. Default class can only be accessed within a package only.
+
+(2) Object Creation :-
+    --> If a class is abstract object creation is not possible, and if a class is not abstract happily you are allowed to create objects.
+     
+(3) Child Class Creation :- 
+    --> if the class is final child class creation is not possible, and vice versa
+
+Which modifiers are applicable to top level class or Outer class :-
+    1) Public
+    2) Default
+    3) Abstract
+    4) Final
+    5) Strictfp (Strict Floating Point)
+
+Which modifiers are applicable to inner class
+    All the above five + 
+    (6) Private
+    (7) Protected
+    (8) Static
+    --> This modifiers are only applicable for inner classes
+ */
